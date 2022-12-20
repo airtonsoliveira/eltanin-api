231bf0d0a24fd5ad4fbbfe8b663ce989195b6df0
Airton <ton020500@gmail.com>
2022-12-20 08:46:01 -0300

    feat: add critiques expectations

--- src/modules/critique/service/GetCritiqueEvaluateService.ts
+++ src/modules/critique/service/GetCritiqueEvaluateService.ts
@@ -13,6 +13,117 @@ export default class GetCritiqueEvaluateService {
         notEvaluated: 2
     }
 
+    private results = [
+        {
+            id: 1,
+            errorMessage: 'Energia compensada deveria existir',
+            successMessage: 'Energia compensada existe',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso II',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 2,
+            errorMessage: 'Energia compensada com valor maior que energia consumida',
+            successMessage: 'Energia compensada com valor menor ou igual à energia consumida',
+            law: 'RN Nº 687 24/11/2015 - Artigo 2º - inciso III',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 3,
+            errorMessage: 'Há energia compensada, porém consumo é zero',
+            successMessage: 'Há energia compensada, porém consumo é maior que zero',
+            law: 'Não há exatamente uma lei que rege este tópico, porém trata-se de um erro, uma vez que compensa-se apenas o que se consome. A energia injetada na rede refere-se como “injetada” ou até rateada',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 4,
+            errorMessage: 'Injeção maior que consumo, porém o saldo de créditos não aumentou',
+            successMessage: 'Injeção maior que consumo e o saldo de créditos aumentou',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - incisos IX, X, XII',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 5,
+            errorMessage: 'Injeção menor que consumo, porém saldo de créditos não diminuiu',
+            successMessage: 'Injeção menor que consumo e saldo de créditos diminuiu ou era zero',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - incisos IX, X, XII',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 6,
+            errorMessage: 'Energia injetada diverge de delta saldo menos energia compensada em mais de 5% ou 100 kWh',
+            successMessage: 'Energia injetada não diverge de delta saldo menos energia compensada em mais de 5% ou 100 kWh',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III, IV',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 7,
+            errorMessage: 'Saldo após compensação incorreto',
+            successMessage: 'Saldo após compensação correto',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 8,
+            errorMessage: 'Energia consumida parcialmente compensada',
+            successMessage: 'Energia consumida devidamente compensada',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 9,
+            errorMessage: 'Energia consumida não compensada, apesar de injeção ou créditos',
+            successMessage: 'Energia consumida compensada com injeção e/ou créditos',
+            law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 10,
+            errorMessage: '',
+            successMessage: '',
+            law: '',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 11,
+            errorMessage: '',
+            successMessage: '',
+            law: '',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+        {
+            id: 12,
+            errorMessage: '',
+            successMessage: '',
+            law: '',
+            status: 2,
+            expected: '',
+            calculed: ''
+        },
+    ]
+
     constructor(dbContext: IDbContext) {
         this.dbContext = dbContext
     }
@@ -34,164 +145,162 @@ export default class GetCritiqueEvaluateService {
             previousCredits: this.credits(previousInvoice.items)
         }
 
-        const result = [
-            {
-                id: 1,
-                errorMessage: 'Energia compensada deveria existir',
-                successMessage: 'Energia compensada existe',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso II',
-                status: this.evalCritique1(aggregate)
-            },
-            {
-                id: 2,
-                errorMessage: 'Energia compensada com valor maior que energia consumida',
-                successMessage: 'Energia compensada com valor menor ou igual à energia consumida',
-                law: 'RN Nº 687 24/11/2015 - Artigo 2º - inciso III',
-                status: this.evalCritique2(aggregate)
-            },
-            {
-                id: 3,
-                errorMessage: 'Há energia compensada, porém consumo é zero',
-                successMessage: 'Há energia compensada, porém consumo é maior que zero',
-                law: 'Não há exatamente uma lei que rege este tópico, porém trata-se de um erro, uma vez que compensa-se apenas o que se consome. A energia injetada na rede refere-se como “injetada” ou até rateada',
-                status: this.evalCritique3(aggregate)
-            },
-            {
-                id: 4,
-                errorMessage: 'Injeção maior que consumo, porém o saldo de créditos não aumentou',
-                successMessage: 'Injeção maior que consumo e o saldo de créditos aumentou',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - incisos IX, X, XII',
-                status: this.evalCritique4(aggregate)
-            },
-            {
-                id: 5,
-                errorMessage: 'Injeção menor que consumo, porém saldo de créditos não diminuiu',
-                successMessage: 'Injeção menor que consumo e saldo de créditos diminuiu ou era zero',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - incisos IX, X, XII',
-                status: this.evalCritique5(aggregate)
-            },
-            {
-                id: 6,
-                errorMessage: 'Energia injetada diverge de delta saldo menos energia compensada em mais de 5% ou 100 kWh',
-                successMessage: 'Energia injetada não diverge de delta saldo menos energia compensada em mais de 5% ou 100 kWh',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III, IV',
-                status: this.evalCritique6(aggregate)
-            },
-            {
-                id: 7,
-                errorMessage: 'Saldo após compensação incorreto',
-                successMessage: 'Saldo após compensação correto',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
-                status: this.evalCritique7(aggregate)
-            },
-            {
-                id: 8,
-                errorMessage: 'Energia consumida parcialmente compensada',
-                successMessage: 'Energia consumida devidamente compensada',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
-                status: this.evalCritique8(aggregate)
-            },
-            {
-                id: 9,
-                errorMessage: 'Energia consumida não compensada, apesar de injeção ou créditos',
-                successMessage: 'Energia consumida compensada com injeção e/ou créditos',
-                law: 'RN Nº 687 24/11/2015 - Artigo 7º - inciso III',
-                status: this.evalCritique9(aggregate)
-            },
-        ]
+        this.evalCritique1(aggregate)
+        this.evalCritique2(aggregate)
+        this.evalCritique3(aggregate)
+        this.evalCritique4(aggregate)
+        this.evalCritique5(aggregate)
+        this.evalCritique6(aggregate)
+        this.evalCritique7(aggregate)
+        this.evalCritique8(aggregate)
+        this.evalCritique9(aggregate)
+        this.evalCritique10(aggregate)
+        this.evalCritique11(aggregate)
+        this.evalCritique12(aggregate)
 
-        return result
+        return this.results
     }
 
     evalCritique1(aggregate: any) {
+        const name = 'Energia compensada existe'
         const consumed = aggregate.consumed
 
+        this.results[0].expected = 'Maior que 0'
+        this.results[0].calculed = consumed
+
         if([consumed].some(value => Number.isNaN(value))) {
-            return this.status.divergent
+            this.results[0].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        if(consumed <= 0) {
+            this.results[0].status = this.status.divergent
+            return
+        }
+        this.results[0].status = this.status.validated
     }
 
     evalCritique2(aggregate: any) {
+        const name = 'Energia compensada com valor menor ou igual à energia consumida'
         const compensed = aggregate.compensed
         const consumed = aggregate.consumed
 
         if([compensed, consumed].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[1].status = this.status.notEvaluated
+            return
         }
+
+        this.results[1].expected = `Menor ou igual a ${consumed}`
+        this.results[1].calculed = compensed
+
         if(compensed > 0 && consumed == 0) {
-            return this.status.divergent
+            this.results[1].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[1].status = this.status.validated
     }
 
     evalCritique3(aggregate: any) {
+        const name = 'Há energia compensada, porém consumo é maior que zero'
         const compensed = aggregate.compensed
         const consumed = aggregate.consumed
 
         if([compensed, consumed].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[2].status = this.status.notEvaluated
+            return
         }
+
+        if(compensed <= 0) {
+            this.results[2].status = this.status.notEvaluated
+            return
+        }
+
+        this.results[2].expected = 'Maior que 0'
+        this.results[2].calculed = consumed
+
         if(compensed > consumed) {
-            return this.status.divergent
+            this.results[2].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[2].status = this.status.validated
     }
 
     evalCritique4(aggregate: any) {
+        const name = 'Injeção maior que consumo e o saldo de créditos aumentou'
         const previousCredits = aggregate.previousCredits
         const credits = aggregate.credits
         const consumed = aggregate.consumed
         const injected = aggregate.injected
 
         if(injected <= consumed || [previousCredits, credits, consumed, injected].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[3].status = this.status.notEvaluated
+            return
         }
+
+        this.results[3].expected = `Maior que ${previousCredits}`
+        this.results[3].calculed = credits
+
         if(previousCredits >= credits) {
-            return this.status.divergent
+            this.results[3].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[3].status = this.status.validated
     }
 
     evalCritique5(aggregate: any) {
+        const name = 'Injeção menor que consumo e saldo de créditos diminuiu ou era zero'
         const previousCredits = aggregate.previousCredits
         const credits = aggregate.credits
         const consumed = aggregate.consumed
         const injected = aggregate.injected
 
         if(injected >= consumed || [previousCredits, credits, consumed, injected].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[4].status = this.status.notEvaluated
+            return
         }
+
+        this.results[4].expected = `Menor que ${previousCredits}`
+        this.results[4].calculed = credits
+
         if(previousCredits <= credits || previousCredits === 0) {
-            return this.status.divergent
+            this.results[4].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[4].status = this.status.validated
     }
 
     evalCritique6(aggregate: any) {
-        return this.status.notEvaluated
+        const name = 'Energia injetada não diverge de delta saldo menos energia compensada em mais de 5% ou 100 kWh'
+        this.results[5].status = this.status.notEvaluated
     }
 
     evalCritique7(aggregate: any) {
+        const name = 'Saldo após compensação correto'
         const previousCredits = aggregate.previousCredits
         const credits = aggregate.credits
         const consumed = aggregate.consumed
         const injected = aggregate.injected
 
         if([previousCredits, credits, consumed, injected].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[6].status = this.status.notEvaluated
+            return
         }
-        let newCredits = previousCredits + injected - consumed
+        let newCredits: any = previousCredits + injected - consumed
         if(newCredits < 0) {
             newCredits = 0
         }
+
+        this.results[6].expected = newCredits
+        this.results[6].calculed = credits
+
         if(newCredits !== credits) {
-            return this.status.divergent
+            this.results[6].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[6].status = this.status.validated
     }
 
     evalCritique8(aggregate: any) {
+        const name = 'Energia consumida devidamente compensada'
         const previousCredits = aggregate.previousCredits
         const credits = aggregate.credits
         const consumed = aggregate.consumed
@@ -199,7 +308,8 @@ export default class GetCritiqueEvaluateService {
         const compensed = aggregate.compensed
 
         if([previousCredits, credits, consumed, injected, compensed].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[7].status = this.status.notEvaluated
+            return
         }
         let calculatedCompensed
         if(consumed - injected > 0) {
@@ -211,13 +321,19 @@ export default class GetCritiqueEvaluateService {
         } else {
             calculatedCompensed = consumed
         }
+
+        this.results[7].expected = calculatedCompensed
+        this.results[7].calculed = compensed
+
         if(calculatedCompensed !== compensed) {
-            return this.status.divergent
+            this.results[7].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[7].status = this.status.validated
     }
 
     evalCritique9(aggregate: any) {
+        const name = 'Energia consumida compensada com injeção e/ou créditos'
         const previousCredits = aggregate.previousCredits
         const credits = aggregate.credits
         const consumed = aggregate.consumed
@@ -225,19 +341,62 @@ export default class GetCritiqueEvaluateService {
         const compensed = aggregate.compensed
 
         if([previousCredits, credits, consumed, injected].some(value => Number.isNaN(value))) {
-            return this.status.notEvaluated
+            this.results[8].status = this.status.notEvaluated
+            return
         }
+
+        this.results[8].expected = 'Maior que 0'
+        this.results[8].calculed = consumed
+
         if([previousCredits, injected].some(value => value > 0) && consumed > 0 && (compensed === 0 || Number.isNaN(compensed))) {
-            return this.status.divergent
+            this.results[8].status = this.status.divergent
+            return
         }
-        return this.status.validated
+        this.results[8].status = this.status.validated
+    }
+
+    evalCritique10(aggregate: any) {
+        const name = 'Não existe fatura do mês anterior'
+        const previousCredits = aggregate.previousCredits
+
+        if([previousCredits].some(value => Number.isNaN(value))) {
+            this.results[9].status = this.status.divergent
+            return
+        }
+
+        this.results[9].status = this.status.validated
+    }
+
+    evalCritique11(aggregate: any) {
+        const name = 'Há consumo ou compensação, mas não há tarifa'
+        const consumed = aggregate.consumed
+        const compensed = aggregate.compensed
+
+        if([consumed, compensed].some(value => Number.isNaN(value))) {
+            this.results[10].status = this.status.notEvaluated
+            return
+        }
+
+        if(consumed <= 0 && compensed <= 0) {
+            this.results[10].status = this.status.notEvaluated
+            return
+        }
+
+        this.results[10].status = this.status.notEvaluated
+        return
+    }
+
+    evalCritique12(aggregate: any) {
+        const name = 'Não há cobrança de consumo ou custo de disponibilidade'
+        this.results[11].status = this.status.notEvaluated
+        return
     }
 
     consumed (items: any[]) {
         const itemsConsumed = items.filter((item: any) => [26, 21, 87, 104, 79, 80].includes(item.id_tipo_item_fatura))
-        
+
         if (itemsConsumed.length === 0) { return NaN }
-    
+
         let sumConsumed = 0
         itemsConsumed.forEach((item: any) => {
             sumConsumed += Number(item.valor)
